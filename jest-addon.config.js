462dd47b863ae88a6e91c2af751a8e2bdfeb6287
nileshgulia1 <nileshgulia@gmail.com>
2022-08-24 15:30:16 +0530

    prettier fix

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  testMatch: ["**/src/addons/**/?(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ['**/src/addons/**/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: [
-    "src/addons/**/src/**/*.{js,jsx,ts,tsx}",
-    "!src/**/*.d.ts",
+    'src/addons/**/src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
   ],
   moduleNameMapper: {
-    "@plone/volto/cypress": "<rootDir>/node_modules/@plone/volto/cypress",
-    "@plone/volto/babel": "<rootDir>/node_modules/@plone/volto/babel",
-    "@plone/volto/(.*)$": "<rootDir>/node_modules/@plone/volto/src/$1",
-    "@package/(.*)$": "<rootDir>/src/$1",
-    "@plone/volto-quanta/(.*)$": "<rootDir>/src/addons/volto-quanta/src/$1",
-    "@eeacms/(.*?)/(.*)$": "<rootDir>/src/addons/$1/src/$2",
-    "@plone/volto-slate":
-      "<rootDir>/node_modules/@plone/volto/packages/volto-slate/src",
-    "~/(.*)$": "<rootDir>/src/$1",
-    "load-volto-addons":
-      "<rootDir>/node_modules/@plone/volto/jest-addons-loader.js",
+    '@plone/volto/cypress': '<rootDir>/node_modules/@plone/volto/cypress',
+    '@plone/volto/babel': '<rootDir>/node_modules/@plone/volto/babel',
+    '@plone/volto/(.*)$': '<rootDir>/node_modules/@plone/volto/src/$1',
+    '@package/(.*)$': '<rootDir>/src/$1',
+    '@plone/volto-quanta/(.*)$': '<rootDir>/src/addons/volto-quanta/src/$1',
+    '@eeacms/(.*?)/(.*)$': '<rootDir>/src/addons/$1/src/$2',
+    '@plone/volto-slate':
+      '<rootDir>/node_modules/@plone/volto/packages/volto-slate/src',
+    '~/(.*)$': '<rootDir>/src/$1',
+    'load-volto-addons':
+      '<rootDir>/node_modules/@plone/volto/jest-addons-loader.js',
   },
   transform: {
-    "^.+\\.js(x)?$": "babel-jest",
-    "^.+\\.css$": "jest-css-modules",
-    "^.+\\.less$": "jest-css-modules",
-    "^.+\\.scss$": "jest-css-modules",
-    "^.+\\.(png)$": "jest-file",
-    "^.+\\.(jpg)$": "jest-file",
-    "^.+\\.(svg)$": "./node_modules/@plone/volto/jest-svgsystem-transform.js",
+    '^.+\\.js(x)?$': 'babel-jest',
+    '^.+\\.css$': 'jest-css-modules',
+    '^.+\\.less$': 'jest-css-modules',
+    '^.+\\.scss$': 'jest-css-modules',
+    '^.+\\.(png)$': 'jest-file',
+    '^.+\\.(jpg)$': 'jest-file',
+    '^.+\\.(svg)$': './node_modules/@plone/volto/jest-svgsystem-transform.js',
   },
   coverageThreshold: {
     global: {
