c92ea95af190b1a9b02328b1b9eb7326a01676e3
Alin Voinea <contact@avoinea.com>
2022-10-24 17:33:25 +0300

    test(sonar): Cleanup code smell

--- src/editor/LabelWrapper.jsx
+++ src/editor/LabelWrapper.jsx
@@ -14,37 +14,41 @@ const LabelWrapper = (props) => {
     data.tooltip_content &&
     serializeNodesToText(data.tooltip_content).trim().length > 0;
 
-  return isTooltipText ? (
-    <Popup
-      position={data.tooltip_pointing}
-      open={data.always_show || undefined}
-      on={!data.always_show && 'click'}
-      trigger={
-        label_type ? (
-          <label
-            className={cx(label_type, label_pointing, 'with-popup ui label')}
-          >
-            {children}
-          </label>
-        ) : (
-          <span
-            id={`label_ref-${uid}`}
-            {...attributes}
-            className="label-node with-popup"
-          >
-            {children}
-          </span>
-        )
-      }
-      className={data.tooltip_type}
-    >
-      {serializeNodes(data.tooltip_content)}
-    </Popup>
-  ) : (
-    <label className={cx(label_type, label_pointing, 'ui label')}>
-      {children}
-    </label>
-  );
+  if (isTooltipText) {
+    return (
+      <Popup
+        position={data.tooltip_pointing}
+        open={data.always_show || undefined}
+        on={!data.always_show && 'click'}
+        trigger={
+          label_type ? (
+            <label
+              className={cx(label_type, label_pointing, 'with-popup ui label')}
+            >
+              {children}
+            </label>
+          ) : (
+            <span
+              id={`label_ref-${uid}`}
+              {...attributes}
+              className="label-node with-popup"
+            >
+              {children}
+            </span>
+          )
+        }
+        className={data.tooltip_type}
+      >
+        {serializeNodes(data.tooltip_content)}
+      </Popup>
+    );
+  } else {
+    return (
+      <label className={cx(label_type, label_pointing, 'ui label')}>
+        {children}
+      </label>
+    );
+  }
 };
 
 export default LabelWrapper;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { LABEL } from './constants';
 import installLabelEditor from './editor';
-//import RichTextWidgetView from 'volto-slate/widgets/RichTextWidgetView';
 
 export default function install(config) {
   config.settings.labels = [...(config.settings.labels || []), LABEL];
-  //config.widgets.views.widget.slate = RichTextWidgetView;
   config = installLabelEditor(config);
-
   return config;
 }
