12a01abae8a2457578e7c44b7e55a4d8aaebe032
kreafox <krisztina.n.elekes@gmail.com>
2023-09-19 10:52:36 +0300

    Add tooltip size option

--- src/editor/LabelWrapper.jsx
+++ src/editor/LabelWrapper.jsx
@@ -9,46 +9,58 @@ import {
 const LabelWrapper = (props) => {
   const { attributes, children, element } = props;
   const { data = {} } = element;
-  const { uid, label_type, label_pointing } = data;
-  const isTooltipText =
-    data.tooltip_content &&
-    serializeNodesToText(data.tooltip_content).trim().length > 0;
+  const {
+    uid,
+    always_show,
+    label_type,
+    label_pointing,
+    tooltip_type,
+    tooltip_size,
+    tooltip_content,
+    tooltip_pointing,
+  } = data;
 
-  if (isTooltipText) {
-    return (
-      <Popup
-        position={data.tooltip_pointing}
-        open={data.always_show || undefined}
-        on={!data.always_show ? 'click' : undefined}
-        trigger={
-          label_type !== undefined && label_type !== 'simple' ? (
-            <label
-              className={cx(label_type, label_pointing, 'with-popup ui label')}
-            >
-              {children}
-            </label>
-          ) : (
-            <span
-              id={`label_ref-${uid}`}
-              {...attributes}
-              className="label-node with-popup"
-            >
-              {children}
-            </span>
-          )
-        }
-        className={data.tooltip_type}
-      >
-        {serializeNodes(data.tooltip_content)}
-      </Popup>
-    );
-  } else {
-    return (
-      <label className={cx(label_type, label_pointing, 'ui label')}>
-        {children}
-      </label>
-    );
-  }
+  const isTooltipText =
+    tooltip_content && serializeNodesToText(tooltip_content).trim().length > 0;
+
+  return isTooltipText ? (
+    <Popup
+      wide={
+        tooltip_size === 'wide'
+          ? true
+          : tooltip_size === 'extra'
+          ? 'very'
+          : null
+      }
+      position={tooltip_pointing}
+      open={always_show || undefined}
+      on={!always_show ? 'click' : undefined}
+      trigger={
+        label_type !== undefined && label_type !== 'simple' ? (
+          <label
+            className={cx(label_type, label_pointing, 'with-popup ui label')}
+          >
+            {children}
+          </label>
+        ) : (
+          <span
+            id={`label_ref-${uid}`}
+            {...attributes}
+            className="label-node with-popup"
+          >
+            {children}
+          </span>
+        )
+      }
+      className={tooltip_type}
+    >
+      {serializeNodes(tooltip_content)}
+    </Popup>
+  ) : (
+    <label className={cx(label_type, label_pointing, 'ui label')}>
+      {children}
+    </label>
+  );
 };
 
 export default LabelWrapper;
